88b6681925a48bfa45e61ab11c6fc8579c75561d
Hiroto-Terai <hiro08051610@gmail.com>
2021-01-09 20:04:10 +0900

    ダイスの画像を表示させ、画面いっぱいに表示されるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,38 @@ class MyApp extends StatelessWidget {
           // ヘッダーの影を削除
           elevation: 0.0,
         ),
-        // 中身。Centerで中央揃え
-        body: Center(
-          child: Container(
-            child: Row(),
-            // 背景を指定
-            color: Colors.red,
-            width: 1000,
-            height: 1000,
-          ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var leftDiceNumber = 3;
+    return Center(
+      child: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                  Expanded(
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
+        // 背景を指定
+        color: Colors.red,
+        width: 1000,
+        height: 1000,
       ),
     );
   }
