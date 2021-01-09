7c9bac546748a50afc4a8a028d39054ec37d7839
Hiroto-Terai <hiro08051610@gmail.com>
2021-01-09 18:48:24 +0900

    全体の配色を統一した

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
+        brightness: Brightness.dark,
       ),
-      home: DicePage(),
-    );
-  }
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("hello World!"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World!',
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+          elevation: 0.0,
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.red,
+            width: 1000,
+            height: 1000,
+          ),
         ),
       ),
     );
